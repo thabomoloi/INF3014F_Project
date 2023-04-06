33810dc8b20c8c0ee13a54769b27b4143c9ea3a9
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-06 18:20:22 +0200

    Add migration

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-
+from flask_migrate import Migrate
 from oasis_nourish import create_app, db, User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
