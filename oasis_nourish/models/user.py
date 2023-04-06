7ed3d4d5ea4a8a44c4ba2dda4f27c810c2bcd823
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-06 18:37:43 +0200

    Add password hashing

--- oasis_nourish/models/user.py
+++ oasis_nourish/models/user.py
@@ -10,7 +10,18 @@ class User(db.Model):
     last_name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.email
-
