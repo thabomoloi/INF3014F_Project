645d4c32815c2d293e8b48977f08c34ca23f155e
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-06 13:23:34 +0200

    Add shell context

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
-from oasis_nourish import create_app, db
+from oasis_nourish import create_app, db, User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db)
+    return dict(db=db, User=User, Role=Role)
