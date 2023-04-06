de95fafdfb8964545fae6c5682923d7e6a97aace
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-06 13:02:11 +0200

    Add user model

---
+++ oasis_nourish/models/user.py
@@ -0,0 +1,15 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .. import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
