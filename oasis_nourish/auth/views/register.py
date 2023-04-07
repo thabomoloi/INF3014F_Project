eb76322693043e3b8ed7aebd73d544fef5089413
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-07 16:31:00 +0200

    Add send mail

--- oasis_nourish/auth/views/register.py
+++ oasis_nourish/auth/views/register.py
@@ -2,7 +2,8 @@ from ..forms import RegistrationForm
 from oasis_nourish import db
 from flask import render_template, redirect, url_for, flash
 from oasis_nourish.models import User
-from  .. import auth
+from .. import auth
+from oasis_nourish.email import send_email
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -17,6 +18,8 @@ def register():
         )
         db.session.add(user)
         db.session.commit()
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account', 'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
