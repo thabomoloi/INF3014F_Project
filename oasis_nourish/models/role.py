4dfc220631161f849fb6a2eb0494d52628f0f266
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-07 16:44:38 +0200

    Add constructor

--- oasis_nourish/models/role.py
+++ oasis_nourish/models/role.py
@@ -5,7 +5,14 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, defualt=False, index=True)
+    permissions = db.Column(db.String)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __int__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
 
     def __repr__(self):
         return '<Role %r>' % self.name
