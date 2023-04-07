ec6f0a8e8bc216e43b54ba542c0c557499c64680
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-07 17:52:52 +0200

    Add context processor for Permission

--- oasis_nourish/main/__init__.py
+++ oasis_nourish/main/__init__.py
@@ -4,4 +4,9 @@ main = Blueprint('main', __name__)
 
 from .views import *
 from .errors import *
+from ..models import Permission
 
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
