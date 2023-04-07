267b77d17604aa0cb3c70b2a2775d7e7e9eef60f
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-07 17:13:11 +0200

    Add insert roles

--- oasis_nourish/models/role.py
+++ oasis_nourish/models/role.py
@@ -1,5 +1,5 @@
 from .. import db
-
+from .permissions import Permission
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -16,3 +16,37 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': [Permission.CHECKOUT, Permission.REVIEW],
+            'Moderator': [Permission.CHECKOUT, Permission.REVIEW, Permission.MODERATE],
+            'Administrator': [Permission.CHECKOUT, Permission.REVIEW, Permission.MODERATE, Permission.ADMIN]
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permissions(self, perm):
+        return self.permissions & perm == perm
+
