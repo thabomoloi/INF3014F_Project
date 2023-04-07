519585f02b82163f2b71287e3cb2821ad780eb4b
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-07 15:06:50 +0200

    Add registration

---
+++ oasis_nourish/auth/views/register.py
@@ -0,0 +1,22 @@
+from ..forms import RegistrationForm
+from oasis_nourish import db
+from flask import render_template, redirect, url_for, flash
+from oasis_nourish.models import User
+from  .. import auth
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
