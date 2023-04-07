65b002b64584acc26540290702aef85ef5413629
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-07 14:10:21 +0200

    Add logout route

---
+++ oasis_nourish/auth/views/logout.py
@@ -0,0 +1,11 @@
+from .. import auth
+from flask_login import logout_user, login_required
+from flask import flash, url_for, redirect
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
