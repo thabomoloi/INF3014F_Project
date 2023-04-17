9c7b18a57b57596c6d7d6f587867d53c9fdfacd5
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-17 12:32:04 +0200

    Add phone and its validation

--- oasis_nourish/auth/forms/registration_form.py
+++ oasis_nourish/auth/forms/registration_form.py
@@ -3,13 +3,14 @@ from wtforms import StringField, PasswordField, EmailField, BooleanField, Submit
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from oasis_nourish.models import User
-
+import phonenumbers
 
 class RegistrationForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired(), Length(1, 64)])
     last_name = StringField('Last name', validators=[DataRequired(), Length(1, 64)])
     email = EmailField('Email', validators=[Email(), DataRequired(), Length(min=1, max=64)])
-    password = PasswordField('Email', validators=[
+    phone = StringField('Phone', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match'), Length(8, 16)
     ])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
@@ -18,3 +19,11 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('Email already registered')
+
+    def validate_phone(self, phone):
+        try:
+            p = phonenumbers.parse(phone.data, "ZA")
+            if not phonenumbers.is_valid_number(p):
+                raise ValueError()
+        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+            raise ValidationError('Invalid phone number')
