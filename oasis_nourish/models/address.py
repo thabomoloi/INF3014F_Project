8056d6cb56ff9668200fc5091274865eea33b030
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-19 16:53:08 +0200

    Define an address class

---
+++ oasis_nourish/models/address.py
@@ -0,0 +1,16 @@
+from .. import db
+
+
+class Address(db.Model):
+    __tablename__ = 'addresses'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    street = db.Column(db.String(64))
+    suburb = db.Column(db.String(64))
+    city = db.Column(db.String(64))
+    postal_code = db.Column(db.String(4))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __int__(self, **kwargs):
+        super(Address, self).__init__(**kwargs)
+
