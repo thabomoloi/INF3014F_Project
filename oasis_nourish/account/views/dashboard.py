1b608dafa5f03fd94338ecc785c3b80a7cca14e6
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-04-20 11:23:40 +0200

    Add route

--- oasis_nourish/account/views/dashboard.py
+++ oasis_nourish/account/views/dashboard.py
@@ -5,6 +5,7 @@ from .. import account
 
 
 @account.route('/dashboard')
+@account.route('/')
 @login_required
 def dashboard():
     return render_template('account/dashboard.html', title='Dashboard')
